174603d675ab6287ce17fbc5761d152aa6402026
Giomar Morales Ramirez <giomar.mr02@gmail.com>
2022-07-19 18:37:45 -0500

    Limpieza de código segun indicaciones del tutor

--- js/index.js
+++ js/index.js
@@ -2,10 +2,9 @@ alert("Hola, en este ejercicio calcularemos cuantas pastillas debes comprar")
 
 let horaToma = parseInt(prompt("¿Cada cuantas horas debes tomar la pastilla?"))
 let dias = parseInt(prompt("¿Por cuantos días debes tomar la pastilla?"))
-
-let pastillasDiarias = parseInt("0")
 let pastillas =" pastillas"
-let pastillasTotales = parseInt("0")
+let pastillasTotales = 0;
+
 
 while (isNaN(horaToma) || isNaN(dias)) {
     alert("Por favor escriba un valor numérico")
@@ -13,7 +12,7 @@ while (isNaN(horaToma) || isNaN(dias)) {
     dias = parseInt(prompt("¿Por cuantos días debes tomar la pastilla?"))
 }
 
-pastillasDiarias = parseInt(24 / horaToma)
+let pastillasDiarias = 24 / horaToma
 pastillasTotales = pastillasDiarias * dias
 pastillasTotales = pastillasTotales+pastillas
 
